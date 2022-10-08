f9ca935c5e28531f55c80cff0c5f754d442bf1ad
FujisawaAkifumi <112544215+fujisawa1234@users.noreply.github.com>
2022-10-08 06:34:40 +0000

    [add]非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,13 +4,13 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_params)
     comment.book_id = book.id
     comment.save
-    redirect_to request.referer
+    render :index
   end
 
   def destroy
     book = BookComment.find(params[:id])
     book.destroy
-    redirect_to book_path(params[:book_id])
+    render :index
   end
 
   private
